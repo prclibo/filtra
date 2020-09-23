update fixed 1x1 kernel rotation

perf/evaluate_stability.py
@@ -14,7 +14,7 @@ import sscnn.e2cnn
 import numpy as np
 
 from mnist_rot_dataset import RotatedMNISTDataset 
-from models import C8Backbone3x3, ClassificationHead, RegressionHead
+from models import C8Backbone3x3, Backbone5x5, ClassificationHead, RegressionHead
 
 import matplotlib.pyplot as plt
 from sscnn.utils import *
@@ -26,10 +26,14 @@ torch.set_printoptions(sci_mode=False, linewidth=160)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 batch_size = 128
 
-conv_func = nn.R2Conv
+group=gspaces.Rot2dOnR2(N=8)
+# group=gspaces.FlipRot2dOnR2(N=8)
+
+# conv_func = nn.R2Conv
 conv_func = sscnn.e2cnn.SSConv
 # conv_func = sscnn.e2cnn.PlainConv
-backbone = C8Backbone3x3(out_channels=2, conv_func=conv_func)
+# backbone = C8Backbone3x3(out_channels=2, conv_func=conv_func)
+backbone = Backbone5x5(out_channels=2, conv_func=conv_func, group=group)
 head = RegressionHead(backbone.out_type, conv_func)
 model = nn.SequentialModule(OrderedDict([
     ('backbone', backbone), ('head', head)
@@ -37,18 +41,22 @@ model = nn.SequentialModule(OrderedDict([
 model = model.to(device)
 model = model.export()
 
-# sdict = torch.load(f'/mnt/workspace/sscnn/orient_state_{conv_func.__name__}.pth')
-sdict = torch.load(f'/home/li/workspace/sscnn/orient_state_{conv_func.__name__}.pth')
+# sdict = torch.load(f'/mnt/workspace/sscnn/orient_state_{conv_func.__name__}.pth.3x3')
+# sdict = torch.load(f'/mnt/workspace/sscnn/orient_state_{conv_func.__name__}.pth.5x5')
+# sdict = torch.load(f'/mnt/workspace/sscnn/orient_state_PlainConv.C8Backbone5x5.8.81136703491211.pth')
+# sdict = torch.load(f'/mnt/workspace/sscnn/orient_state_R2Conv.C8Backbone5x5.7.28338098526001.pth')
+# sdict = torch.load(f'/mnt/workspace/sscnn/orient_state_SSConv.pth.5x5.bilinear')
+sdict = torch.load(f'/mnt/workspace/sscnn/orient_state_SSConv.Rot2dOnR2.6.255855083465576.pth')
 model.load_state_dict(sdict)
 
-model = model.backbone.block1
+# model = model.backbone.block1
 
 height, width = 33, 33
 x = torch.rand(1, 1, height, width).to(device)
 y0 = model(x)#.flatten()
 # start = torch.atan2(y0[1], y0[0])
 
-NN = 8
+NN = 16
 # for i in range(NN):
 #     angle = np.pi * 2 / NN * i
 #     aff = torch.zeros(1, 2, 3)
@@ -64,20 +72,64 @@ NN = 8
 # 
 # print(y0, y1)
 # import pdb; pdb.set_trace()
-x = torch.rand(1, 1, height, width).to(device).expand(NN, -1, -1, -1)
+mnist_test = RotatedMNISTDataset('.', train=False, random_rotate=False)
+
+patch = torch.from_numpy(np.load('/mnt/workspace/sscnn/test/patch.npy'))
+x, _, _ = mnist_test[0]
+x = x.unsqueeze(0).to(device).expand(NN, -1, -1, -1)
+x = patch.unsqueeze(0).unsqueeze(0).to(device).expand(NN, -1, -1, -1)
 y0 = model(x[0:1]).flatten()
 start = torch.atan2(y0[1], y0[0])
 
 
 angles = torch.arange(NN).float() / NN * np.pi * 2
-rotated = batch_rotate(x, angles)
-y = model(rotated)
+
+
+
+# errors = []
+# for i in range(0, len(mnist_test), 100):
+#     x, _, _ = mnist_test[i]
+#     x = x.unsqueeze(0).to(device).expand(NN, -1, -1, -1)
+#     rotated = batch_rotate(x, angles)
+#     y = model(rotated)
+# 
+#     pred_angles = torch.atan2(y[:, 1, 0, 0], y[:, 0, 0, 0]) - start
+#     pred_angles += np.pi * 2
+#     pred_angles[-1] -= 0.01
+#     pred_angles[0] += 0.01
+#     pred_angles = pred_angles.fmod(np.pi * 2)
+# 
+#     error = (pred_angles.cpu() - angles.cpu()).abs().sum().item() / np.pi * 180 / (NN - 4)
+#     print(i, error)
+#     errors.append(error)
+# 
+# print(np.array(errors).mean())
 # import pdb; pdb.set_trace()
 
-pred_angles = torch.atan2(y[:, 1, 0, 0], y[:, 0, 0, 0])
-# pred_angles = (pred_angles + np.pi * 2).fmod(np.pi * 2)
+rotated = batch_rotate(x, angles)
+y = model(rotated)
+
+# import matplotlib.pyplot as plt
+# for i in range(NN):
+#     fig = plt.figure()
+#     plt.imshow(rotated[i, 0].detach().cpu())
+# 
+# plt.show()
+
+# import pdb; pdb.set_trace()
+
+pred_angles = torch.atan2(y[:, 1, 0, 0], y[:, 0, 0, 0]) - start
+pred_angles += np.pi * 2
+pred_angles[-1] -= 0.01
+pred_angles[0] += 0.01
+pred_angles = pred_angles.fmod(np.pi * 2)
 
 print(y[:, :, 0, 0])
-print(pred_angles - start)
+print(pred_angles)
+print((pred_angles.cpu() - angles.cpu()).abs().sum())
+
+import matplotlib.pyplot as plt
+plt.plot(angles.cpu().detach(), pred_angles.cpu().detach())
+plt.show()
 
 import pdb; pdb.set_trace()

perf/mnist_orient.py
@@ -1,6 +1,9 @@
 import sys
 sys.path.append('/mnt/workspace/sscnn/')
 
+import os
+import time
+
 from collections import OrderedDict
 import torch
 import torch.nn.functional as F
@@ -14,17 +17,24 @@ import sscnn.e2cnn
 import numpy as np
 
 from mnist_rot_dataset import RotatedMNISTDataset 
-from models import C8Backbone3x3, ClassificationHead, RegressionHead
+from models import C8Backbone3x3, Backbone5x5, ClassificationHead, RegressionHead
 
 import matplotlib.pyplot as plt
+
+torch.manual_seed(23)
+torch.cuda.manual_seed(23)
+np.random.seed(23)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 batch_size = 128
 
+group=gspaces.Rot2dOnR2(N=8)
+# group=gspaces.FlipRot2dOnR2(N=8)
+
 conv_func = nn.R2Conv
-# conv_func = sscnn.e2cnn.SSConv
+conv_func = sscnn.e2cnn.SSConv
 # conv_func = sscnn.e2cnn.PlainConv
-backbone = C8Backbone3x3(out_channels=2, conv_func=conv_func)
+backbone = Backbone5x5(out_channels=2, conv_func=conv_func, group=group)
 head = RegressionHead(backbone.out_type, conv_func)
 model = nn.SequentialModule(OrderedDict([
     ('backbone', backbone), ('head', head)
@@ -38,14 +48,18 @@ mnist_test = RotatedMNISTDataset('.', train=False)
 test_loader = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size)
 
 loss_function = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=5e-5, weight_decay=1e-5)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=1e-5)
 
+file_path = None
 for epoch in range(41):
     model.train()
 
-    start = torch.cuda.Event(enable_timing=True)
-    end = torch.cuda.Event(enable_timing=True)
-    start.record()
+    if device == 'cuda':
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
+        start.record()
+    else:
+        start = time.time()
 
     for i, (x, l, v) in enumerate(train_loader):
         
@@ -56,6 +70,7 @@ for epoch in range(41):
 
         y = model(nn.GeometricTensor(x, backbone.input_type))
         y = y.tensor.flatten(1, -1)
+
         y = F.normalize(y, dim=1)
 
         loss = loss_function(y, v)
@@ -63,15 +78,17 @@ for epoch in range(41):
         loss.backward()
 
         optimizer.step()
-        # break
+        # if i > 50:
+        #     break
 
-    end.record()
-    torch.cuda.synchronize()
-    print('Epoch', start.elapsed_time(end))
+    if device == 'cuda':
+        end.record()
+        torch.cuda.synchronize()
+        print('Epoch', start.elapsed_time(end))
+    else:
+        end = time.time()
+        print('Epoch', end - start)
 
-    exported = model.export()
-    torch.save(exported.state_dict(), f'./orient_state_{conv_func.__name__}.pth')
-    
     if epoch % 5 == 0:
         total = 0
         error = 0
@@ -92,4 +109,13 @@ for epoch in range(41):
                 error = angles.sum() + error
 
                 loss = loss_function(y, v)
-        print(f"epoch {epoch} | tes : {error/total}, {loss}")
+        error = error/total
+        print(f"epoch {epoch} | tes : {error}, {loss}")
+        exported = model.export()
+        print(file_path)
+        if file_path:
+            print('removing')
+            os.remove(file_path)
+        file_path = f'./orient_state_{conv_func.__name__}.{type(group).__name__}.{error}.pth'
+        torch.save(exported.state_dict(), file_path)
+    

perf/mnist_rot_dataset.py
@@ -14,16 +14,52 @@ def batch_rotate(images, angles):
     aff[:, 0, 1] = -sina
     aff[:, 1, 0] = sina
     aff[:, 1, 1] = cosa
-    print(aff)
     grid = F.affine_grid(aff, images.shape, False)
     rotated = F.grid_sample(images, grid, align_corners=False, 
             padding_mode='zeros', mode='bilinear') 
     return rotated
 
+class RotatedDataset(Dataset):
+    def __init__(self, dataset, random_rotate=True):
+        super(RotatedDataset, self).__init__()
+        self.dataset = dataset
+        self.random_rotate = random_rotate
+
+    def _rotate_data(self, angle, image):
+        aff = torch.zeros(1, 2, 3)
+        aff[0, :, :2] = rot_mat(angle)
+        grid = F.affine_grid(aff, (1, 1) + image.shape, False)
+
+        rotated = F.grid_sample(image[None, None, ...], grid, align_corners=False,
+                padding_mode='zeros', mode='bilinear') 
+
+        return rotated[0]
+
+    def __getitem__(self, index):
+        image, label = self.dataset[index].float(), int(self.targets[index])
+
+        if self.random_rotate:
+            angle = np.random.uniform(2 * np.pi)
+        else:
+            angle = 0.0
+        # image = torch.from_numpy(image)# .transpose(0, 1)
+        rotated = self._rotate_data(angle, image)
+        orient = torch.Tensor([np.cos(angle), np.sin(angle)])
+
+        # import matplotlib.pyplot as plt
+        # plt.imshow(torch.cat([image, rotated[0]]).cpu())
+        # plt.show()
+        # print(orient)
+
+        # import pdb; pdb.set_trace()
+
+        return rotated, label, orient
+
 class RotatedMNISTDataset(torchvision.datasets.MNIST):
-    def __init__(self, root, train):
+    def __init__(self, root, train, random_rotate=True):
         super(RotatedMNISTDataset, self).__init__(root, train, download=True)
         print(self.data.shape, self.targets.shape)
+        self.random_rotate = random_rotate
 
     def _rotate_data(self, angle, image):
         aff = torch.zeros(1, 2, 3)
@@ -38,7 +74,10 @@ class RotatedMNISTDataset(torchvision.datasets.MNIST):
     def __getitem__(self, index):
         image, label = self.data[index].float(), int(self.targets[index])
 
-        angle = np.random.uniform(2 * np.pi)
+        if self.random_rotate:
+            angle = np.random.uniform(2 * np.pi)
+        else:
+            angle = 0.0
         # image = torch.from_numpy(image)# .transpose(0, 1)
         rotated = self._rotate_data(angle, image)
         orient = torch.Tensor([np.cos(angle), np.sin(angle)])

perf/models.py
@@ -121,12 +121,13 @@ class C8Backbone3x3(nn.SequentialModule):
 
         self.out_type = out_type
 
-class C8Backbone5x5(nn.SequentialModule):
-    def __init__(self, out_channels, conv_func):
-        super(C8Backbone, self).__init__()
+class Backbone5x5(nn.SequentialModule):
+    def __init__(self, out_channels, conv_func, group):
+        super(Backbone5x5, self).__init__()
 
         # the model is equivariant under rotations by 45 degrees, modelled by C8
-        self.r2_act = gspaces.Rot2dOnR2(N=8)
+        # self.r2_act = gspaces.Rot2dOnR2(N=8)
+        self.r2_act = group
 
         # the input image is a scalar field, corresponding to the trivial representation
         in_type = nn.FieldType(self.r2_act, [self.r2_act.trivial_repr])
@@ -137,7 +138,7 @@ class C8Backbone5x5(nn.SequentialModule):
         # convolution 1
         # first specify the output type of the convolutional layer
         # we choose 16 feature fields, each transforming under the regular representation of C8
-        out_type = nn.FieldType(self.r2_act, 24*[self.r2_act.regular_repr])
+        out_type = nn.FieldType(self.r2_act, 16*[self.r2_act.regular_repr])
         self.add_module('block1', nn.SequentialModule(
             # nn.MaskModule(in_type, 29, margin=1),
             conv_func(in_type, out_type, kernel_size=5, padding=1, bias=False),
@@ -149,7 +150,7 @@ class C8Backbone5x5(nn.SequentialModule):
         # the old output type is the input type to the next layer
         in_type = out_type
         # the output type of the second convolution layer are 32 regular feature fields of C8
-        out_type = nn.FieldType(self.r2_act, 48*[self.r2_act.regular_repr])
+        out_type = nn.FieldType(self.r2_act, 24*[self.r2_act.regular_repr])
         self.add_module('block2', nn.SequentialModule(
             conv_func(in_type, out_type, kernel_size=5, padding=2, bias=False),
             # nn.InnerBatchNorm(out_type),
@@ -163,7 +164,7 @@ class C8Backbone5x5(nn.SequentialModule):
         # the old output type is the input type to the next layer
         in_type = out_type
         # the output type of the third convolution layer are 32 regular feature fields of C8
-        out_type = nn.FieldType(self.r2_act, 48*[self.r2_act.regular_repr])
+        out_type = nn.FieldType(self.r2_act, 32*[self.r2_act.regular_repr])
         self.add_module('block3', nn.SequentialModule(
             conv_func(in_type, out_type, kernel_size=5, padding=2, bias=False),
             # nn.InnerBatchNorm(out_type),
@@ -174,7 +175,7 @@ class C8Backbone5x5(nn.SequentialModule):
         # the old output type is the input type to the next layer
         in_type = out_type
         # the output type of the fourth convolution layer are 64 regular feature fields of C8
-        out_type = nn.FieldType(self.r2_act, 96*[self.r2_act.regular_repr])
+        out_type = nn.FieldType(self.r2_act, 48*[self.r2_act.regular_repr])
         self.add_module('block4', nn.SequentialModule(
             conv_func(in_type, out_type, kernel_size=5, padding=2, bias=False),
             # nn.InnerBatchNorm(out_type),
@@ -188,7 +189,7 @@ class C8Backbone5x5(nn.SequentialModule):
         # the old output type is the input type to the next layer
         in_type = out_type
         # the output type of the fifth convolution layer are 64 regular feature fields of C8
-        out_type = nn.FieldType(self.r2_act, 96*[self.r2_act.regular_repr])
+        out_type = nn.FieldType(self.r2_act, 64*[self.r2_act.regular_repr])
         self.add_module('block5', nn.SequentialModule(
             conv_func(in_type, out_type, kernel_size=5, padding=2, bias=False),
             # nn.InnerBatchNorm(out_type),
@@ -199,7 +200,7 @@ class C8Backbone5x5(nn.SequentialModule):
         # the old output type is the input type to the next layer
         in_type = out_type
         # the output type of the sixth convolution layer are 64 regular feature fields of C8
-        out_type = nn.FieldType(self.r2_act, 64*[self.r2_act.regular_repr])
+        out_type = nn.FieldType(self.r2_act, 96*[self.r2_act.regular_repr])
         self.add_module('block6', nn.SequentialModule(
             conv_func(in_type, out_type, kernel_size=5, padding=1, bias=False),
             # nn.InnerBatchNorm(out_type),
@@ -235,12 +236,18 @@ class RegressionHead(nn.SequentialModule):
         # number of output channels
         # Fully Connected
         in_type = in_type
-        out_type = nn.FieldType(gspace, 8 * [gspace.regular_repr])
+        out_type = nn.FieldType(gspace, 32 * [gspace.regular_repr])
         self.add_module('block1', nn.SequentialModule(
             conv_func(in_type, out_type, kernel_size=1, padding=0, bias=False),
             nn.ReLU(out_type, inplace=True)
         ))
         in_type = out_type
-        out_type = nn.FieldType(gspace, [gspace.irrep(1)])
+        if isinstance(gspace, gspaces.Rot2dOnR2):
+            out_type = nn.FieldType(gspace, [gspace.irrep(1)])
+        elif isinstance(gspace, gspaces.FlipRot2dOnR2):
+            out_type = nn.FieldType(gspace, [gspace.irrep(1, 1)])
+        else:
+            raise NotImplementedError
+
         self.add_module('block2', conv_func(in_type, out_type, kernel_size=1, padding=0, bias=False))
 

sscnn/conv.py
@@ -10,6 +10,9 @@ from torch.nn import functional as F
 from .utils import *
 from .rotater import *
 
+DEFAULT_INTERPOLATION='nearest'
+DEFAULT_INTERPOLATION='bilinear'
+
 def comp_irreps_expan_coeffs(group: Tuple[int, int], irreps: torch.Tensor):
     M = len(irreps)
     angles = torch.arange(group[1]) * math.pi * 2 / group[1]
@@ -39,7 +42,7 @@ def comp_dctmat(group):
 class IrrepToRegular(nn.Conv2d):
     def __init__(self, group: Tuple[int, int],
             in_irreps: List[Tuple[int, int]], out_mult: int,
-            kernel_size: int, **kwargs):
+            kernel_size: int, interpolation=DEFAULT_INTERPOLATION,  **kwargs):
 
         in_channels = len(in_irreps)
         out_channels = out_mult
@@ -56,7 +59,7 @@ class IrrepToRegular(nn.Conv2d):
         self.register_buffer('expand_coeffs', expand_coeffs)
 
         self.filters = None
-        self.rotater = FilterRotater(group, self.kernel_size, reuse=False)
+        self.rotater = FilterRotater(group, self.kernel_size, reuse=False, interpolation=DEFAULT_INTERPOLATION)
 
         # TODO XXX Deal with bias, refering to e2cnn
 
@@ -90,8 +93,8 @@ class IrrepToRegular(nn.Conv2d):
 class RegularToIrrep(IrrepToRegular):
     def __init__(self, group: Tuple[int, int],
             in_mult: int, out_irreps: List[Tuple[int, int]],
-            kernel_size: int, **kwargs):
-        super(RegularToIrrep, self).__init__(group, out_irreps, in_mult, kernel_size, **kwargs)
+            kernel_size: int, interpolation=DEFAULT_INTERPOLATION,  **kwargs):
+        super(RegularToIrrep, self).__init__(group, out_irreps, in_mult, kernel_size, interpolation, **kwargs)
 
     def expand_filters(self, weight):
         # [group[0] x group[1] x in_mult] x [2 x len(out_irreps)] x H x W
@@ -101,10 +104,10 @@ class RegularToIrrep(IrrepToRegular):
 
 class RegularToRegular(IrrepToRegular):
     def __init__(self, group: Tuple[int, int], in_mult:int, out_mult: int,
-            kernel_size: int, **kwargs):
+            kernel_size: int, interpolation=DEFAULT_INTERPOLATION,  **kwargs):
         in_irreps = [(s, r) for s in range(group[0]) for r in range(group[1] // 2 + 1)]
         in_irreps = [irrep for irrep in in_irreps for _ in range(in_mult)]
-        super(RegularToRegular, self).__init__(group, in_irreps, out_mult, kernel_size, **kwargs)
+        super(RegularToRegular, self).__init__(group, in_irreps, out_mult, kernel_size, interpolation,  **kwargs)
 
         self.in_mult = in_mult
         # [group[0] x group[1]] x [2 x len(irreps)]

sscnn/e2cnn.py
@@ -62,6 +62,10 @@ class SSConv(EquivariantModule):
         if isinstance(in_type.gspace, e2cnn.gspaces.Rot2dOnR2):
             assert isinstance(in_type.gspace.fibergroup, e2cnn.group.CyclicGroup)
             group = (1, len(in_type.gspace.fibergroup.elements))
+        elif isinstance(in_type.gspace, e2cnn.gspaces.FlipRot2dOnR2):
+            assert isinstance(in_type.gspace.fibergroup, e2cnn.group.DihedralGroup)
+            group = (2, in_type.gspace.fibergroup.rotation_order)
+            print('group', group)
         else:
             raise NotImplementedError
 
@@ -70,6 +74,7 @@ class SSConv(EquivariantModule):
         self.out_type = out_type
         in_type, self.in_mult, self.in_inner_dim = convert_field_type(in_type)
         out_type, self.out_mult, self.out_inner_dim = convert_field_type(out_type)
+        print('type', in_type, out_type)
 
         in_sel = type_to_selection(in_type, group)
         out_sel = type_to_selection(out_type, group)
@@ -97,14 +102,14 @@ class SSConv(EquivariantModule):
         x_ = x_.view(N, self.in_mult, self.in_inner_dim, H, W)
         x_ = x_.transpose(1, 2).reshape(N, -1, H, W)
 
-        start = torch.cuda.Event(enable_timing=True)
-        end = torch.cuda.Event(enable_timing=True)
-        start.record()
+        # start = torch.cuda.Event(enable_timing=True)
+        # end = torch.cuda.Event(enable_timing=True)
+        # start.record()
 
         x_ = self.conv.forward(x_)
         
-        end.record()
-        torch.cuda.synchronize()
+        # end.record()
+        # torch.cuda.synchronize()
         # print('    ', start.elapsed_time(end))
 
         N, C, H, W = x_.shape
@@ -180,3 +185,6 @@ class PlainConv(EquivariantModule):
         wo = math.floor((wi + 2 * self.padding - self.dilation * (self.kernel_size - 1) - 1) / self.stride + 1)
 
         return b, self.out_type.size, ho, wo
+
+    def export(self):
+        return self.conv

sscnn/rotater.py
@@ -11,7 +11,7 @@ EPS = 1e-5
 LARGE = 1e5
 
 class BMMRemapper(nn.Module):
-    def __init__(self, grid, interpolation='nearest'):
+    def __init__(self, grid, interpolation='bilinear'):
         '''
         Args:
             grid: N x H x W x 2
@@ -29,34 +29,39 @@ class BMMRemapper(nn.Module):
 
     def comp_interp_mat(self, grid):
         N, H, W, _ = grid.shape
-        # H x W, corresponding -1 < nlized_grid < 1
-        disk_mask = (grid[:, :, :, 0] >= 0 - 0.5).all(0) &\
-                    (grid[:, :, :, 0] <= H - 1 + 0.5).all(0) &\
-                    (grid[:, :, :, 1] >= 0 - 0.5).all(0) &\
-                    (grid[:, :, :, 1] <= W - 1 + 0.5).all(0)
-        disk_mask = disk_mask.flatten()
-        num_valid = disk_mask.sum()
-        
-        grid = grid.view(N, -1, 2)
-        # N x num_valid x 2
-        grid = torch.stack([
-            grid[:, disk_mask, 0].clamp(EPS, H - 1 - EPS),
-            grid[:, disk_mask, 1].clamp(EPS, W - 1 - EPS),
-        ], -1)
-
-        if self.interpolation == 'nearest':
-            corners, coeffs = self.comp_interp_mat_helper_nearest(grid, H, W)
-        elif self.interpolation == 'bilinear':
-            corners, coeffs = self.comp_interp_mat_helper_bilinear(grid, H, W)
-        else:
+        if H == 1 and W == 1:
+            return grid.new_ones(N, 1, 1)
+        elif H == 1 or W == 1:
             raise NotImplementedError
+        else:
+            # H x W, corresponding -1 < nlized_grid < 1
+            disk_mask = (grid[:, :, :, 0] >= 0 - 0.5).all(0) &\
+                        (grid[:, :, :, 0] <= H - 1 + 0.5).all(0) &\
+                        (grid[:, :, :, 1] >= 0 - 0.5).all(0) &\
+                        (grid[:, :, :, 1] <= W - 1 + 0.5).all(0)
+            disk_mask = disk_mask.flatten()
+            num_valid = disk_mask.sum()
+            
+            grid = grid.view(N, -1, 2)
+            # N x num_valid x 2
+            grid = torch.stack([
+                grid[:, disk_mask, 0].clamp(EPS, H - 1 - EPS),
+                grid[:, disk_mask, 1].clamp(EPS, W - 1 - EPS),
+            ], -1)
 
-        mat_val  = grid.new_zeros(N, num_valid, H * W + 1)
-        mat_val.scatter_(2, corners, coeffs)
-        # N x [H x W] x [H x W]
-        interp_mat = grid.new_zeros(N, H * W, H * W)
-        interp_mat[:, disk_mask, :] = mat_val[:, :, :-1]
-        return interp_mat
+            if self.interpolation == 'nearest':
+                corners, coeffs = self.comp_interp_mat_helper_nearest(grid, H, W)
+            elif self.interpolation == 'bilinear':
+                corners, coeffs = self.comp_interp_mat_helper_bilinear(grid, H, W)
+            else:
+                raise NotImplementedError
+
+            mat_val  = grid.new_zeros(N, num_valid, H * W + 1)
+            mat_val.scatter_(2, corners, coeffs)
+            # N x [H x W] x [H x W]
+            interp_mat = grid.new_zeros(N, H * W, H * W)
+            interp_mat[:, disk_mask, :] = mat_val[:, :, :-1]
+            return interp_mat
 
     def comp_interp_mat_helper_nearest(self, grid, H, W):
         N, num_valid, _ = grid.shape
@@ -121,11 +126,12 @@ class BMMRemapper(nn.Module):
         return x
 
 class GridSampleRemapper(nn.Module):
-    def __init__(self, nlized_grid):
+    def __init__(self, nlized_grid, interpolation):
         super(GridSampleRemapper, self).__init__()
         disk_mask = (nlized_grid.norm(dim=-1) > 1).unsqueeze(-1)
         nlized_grid = nlized_grid.masked_fill(disk_mask, LARGE)
         nlized_grid = nlized_grid.flatten(0, 1)
+        self.interpolation = interpolation
 
         self.register_buffer('nlized_grid', nlized_grid)
     def forward(self, x):
@@ -133,11 +139,11 @@ class GridSampleRemapper(nn.Module):
         Args:
             x: groups x channels x H x W
         '''
-        x = F.grid_sample(x, self.nlized_grid, align_corners=False, padding_mode='zeros', mode='bilinear')
+        x = F.grid_sample(x, self.nlized_grid, align_corners=False, padding_mode='zeros', mode=self.interpolation)
         return x
 
 class FilterRotater(nn.Module):
-    def __init__(self, group: Tuple[int, int], size: Tuple[int, int], reuse=True, method='bmm'):
+    def __init__(self, group: Tuple[int, int], size: Tuple[int, int], reuse=True, method='bmm', interpolation='bilinear'):
         super(FilterRotater, self).__init__()
         check_validity(group=group)
         self.group = group
@@ -176,9 +182,9 @@ class FilterRotater(nn.Module):
             self.divisor = (1, 1)
         portion = tuple(g // d for g, d in zip(group, self.divisor))
         if method == 'bmm':
-            self.remapper = BMMRemapper(grid[:portion[0], :portion[1]].flatten(0, 1))
+            self.remapper = BMMRemapper(grid[:portion[0], :portion[1]].flatten(0, 1), interpolation)
         elif method == 'grid_sample':
-            self.remapper = GridSampleRemapper(ngrid[:portion[0], :portion[1]])
+            self.remapper = GridSampleRemapper(ngrid[:portion[0], :portion[1]], interpolation)
         else:
             raise NotImplementedError
 